dd1feaf1defbd54e0c235f2ec7dfb9a09661c797
ZuiYee <39691460+ZuiYee@users.noreply.github.com>
2018-12-19 17:04:31 +0800

    12.19.1

--- src/cn/itcast/dao/impl/UserDaoImpl.java
+++ src/cn/itcast/dao/impl/UserDaoImpl.java
@@ -2,16 +2,25 @@ package cn.itcast.dao.impl;
 
 import cn.itcast.dao.UserDao;
 import cn.itcast.domain.User;
+import cn.itcast.util.JDBCUtils;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.util.List;
 
 
 public class UserDaoImpl implements UserDao {
-
+    //实例化
+    private JdbcTemplate template = new JdbcTemplate(JDBCUtils.getDataSource());
 
     @Override
     public List<User> findAll() {
         //使用JDBC操作数据库
-        return null;
+
+        //1.定义sql
+        String sql = "select * from user";
+        List<User> users =  template.query(sql, new BeanPropertyRowMapper<>(User.class));
+
+        return users;
     }
 }
